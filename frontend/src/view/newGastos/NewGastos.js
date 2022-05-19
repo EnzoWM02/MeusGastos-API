c84f1172cd3f1678affc4ac1f63226c257645234
Enzo <enzowm02@pop-os.localdomain>
2022-05-19 15:45:47 -0300

    Adicionada função de cadastrar novo gasto

--- frontend/src/view/newGastos/NewGastos.js
+++ frontend/src/view/newGastos/NewGastos.js
@@ -2,17 +2,34 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import TopMenuBar from "../components/TopMenuBar";
 import {Button} from "reactstrap";
-import TableGastos from "../components/TableGastos";
+import Input from '@mui/material/Input';
 import TextField from "@mui/material/TextField";
 import "./NewGastos.css"
+import { toastContainer, toast } from 'react-toastify';
 import {useNavigate} from "react-router-dom";
 
 const NewGastos = () => {
+
+    const [fieldValues, setFieldValues] = useState({});
 
     const navigate = useNavigate();
 
     const backToMain = async () => {
         navigate('/MainView')
+    }
+
+    const newGasto = async () => {
+        let name = fieldValues.name;
+        let description = fieldValues.description;
+        let value = fieldValues.value;
+        try {
+                const data = await axios.post(process.env.REACT_APP_API_URL_GASTOS, {name, description, value});
+                console.log(data);
+                navigate('/mainView');
+            } catch (e) {
+                toast.error("Os campos não foram corretamente preenchidos");
+                console.log (e);
+            }
     }
 
     return (
@@ -28,6 +45,9 @@ const NewGastos = () => {
                     label="Nome"
                     variant="outlined"
                     type="text"
+                    onChange={(e) => {
+                        setFieldValues({ ...fieldValues, name: e.target.value });
+                    }}
                 />
                 <TextField
                     className="textField"
@@ -35,15 +55,21 @@ const NewGastos = () => {
                     label="Descrição"
                     variant="outlined"
                     type="text"
+                    onChange={(e) => {
+                        setFieldValues({ ...fieldValues, description: e.target.value });
+                    }}
                 />
                 <TextField
                     className="textField"
                     id="value"
                     label="Valor"
                     variant="outlined"
-                    type="value"
+                    Input type="number"
+                    onChange={(e) => {
+                        setFieldValues({ ...fieldValues, value: e.target.value });
+                    }}
                 />
-                <Button className="filledButton" variant="contained" >
+                <Button className="filledButton" variant="contained" onClick={newGasto}>
                     Cadastrar
                 </Button>
                     <Button className="signupButton outlinedButton" variant="outlined" onClick={backToMain}>
