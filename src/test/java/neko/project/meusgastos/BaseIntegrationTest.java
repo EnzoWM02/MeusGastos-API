e2e0f92303354ee297df31d659ba11bbf819e9be
enzowm02 <enzo.mosman@bimachine.com.br>
2022-06-29 14:01:55 -0300

    Adicionado test profile

--- src/test/java/neko/project/meusgastos/BaseIntegrationTest.java
+++ src/test/java/neko/project/meusgastos/BaseIntegrationTest.java
@@ -4,10 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest(properties = {"spring.datasource.url=jdbc:postgresql://localhost:5432/meusgastos"})
+@SpringBootTest
+/*(properties = {
+    "spring.datasource.url=jdbc:postgresql://kesavan.db.elephantsql.com:5432/xaxxmasf",
+    "spring.datasource.username=xaxxmasf",
+    "spring.datasource.password=anOzSpMw6fUENnibG0vQ7MDlxsw17INw"
+})*/
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @Transactional
 public abstract class BaseIntegrationTest{
